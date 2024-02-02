af6ce33b9c9842454c1bbca6ffa5b572c78e015b
mumbereh <siirasonh@gmail.com>
2024-02-02 04:04:11 +0300

    fixed all required changes

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,6 +1,6 @@
 # app/controllers/root_controller.rb
 class RootController < ApplicationController
   def index
-    @random_greeting = Greeting.order("RANDOM()").first
+    @random_greeting = Greeting.order('RANDOM()').first
   end
 end
